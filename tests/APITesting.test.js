5de0e107a80b181023e6f38830f6632f20c74d0d
ronabet <ronabet123@gmail.com>
2019-10-23 16:39:19 +0300

    Get requests tests

--- tests/APITesting.test.js
+++ tests/APITesting.test.js
@@ -1,4 +1,4 @@
-const { expect } = require("chai");
+// const { expect } = require("chai");
 const { Server } = require("../server");
 const request = require("supertest");
 
@@ -24,4 +24,139 @@ describe("router", function() {
       });
     });
   });
+  describe("#GET /api/getAllGroups", function() {
+    context("when all groups returned", function() {
+      it("should return groups details", function(done) {
+        this.timeout(10000);
+        request(server.app)
+          .get("/api/getAllGroups")
+          .expect(200)
+          .end((err, res) => {
+            if (err) {
+              done(err);
+            }
+            done();
+          });
+      });
+    });
+  });
+  describe("#GET /api/getAllGroups", function() {
+    context("when all groups returned", function() {
+      it("should return groups details", function(done) {
+        this.timeout(10000);
+        request(server.app)
+          .get("/api/getAllGroups")
+          .expect(200)
+          .end((err, res) => {
+            if (err) {
+              done(err);
+            }
+            done();
+          });
+      });
+    });
+  });
+  describe("#GET /api/allGroups/getGroupsByPersonAdmin/:id", function() {
+    context("when groups by admin returned", function() {
+      it("should return admin groups", function(done) {
+        this.timeout(10000);
+        request(server.app)
+          .get("/api/allGroups/getGroupsByPersonAdmin/5d4689e4c625210a80202ba7")
+          .expect(200)
+          .end((err, res) => {
+            if (err) {
+              done(err);
+            }
+            done();
+          });
+      });
+    });
+  });
+  describe("#GET /api/allGroups/getGroupsByPersonNotAdmin/:id", function() {
+    context("when groups by not admin returned", function() {
+      it("should return Not admin groups", function(done) {
+        this.timeout(10000);
+        request(server.app)
+          .get("/api/allGroups/getGroupsByPersonNotAdmin/5d2594e36fcb691a0d178a71")
+          .expect(200)
+          .end((err, res) => {
+            if (err) {
+              done(err);
+            }
+            done();
+          });
+      });
+    });
+  });
+  describe("#GET /api/getOneGroupById/:id", function() {
+    context("when Id group return all group details", function() {
+      it("should return group details by ID", function(done) {
+        this.timeout(10000);
+        request(server.app)
+          .get("/api/getOneGroupById/5d46a9c76533f31bb00a961c")
+          .expect(200)
+          .end((err, res) => {
+            if (err) {
+              done(err);
+            }
+            done();
+          });
+      });
+    });
+  });
+  describe("#GET /api/getAllGroupMembersByID/:id", function() {
+    context("when Id group return all members of group ", function() {
+      it("should return group members", function(done) {
+        this.timeout(10000);
+        request(server.app)
+          .get("/api/getAllGroupMembersByID/5d46a9c76533f31bb00a961c")
+          .expect(200)
+          .end((err, res) => {
+            if (err) {
+              done(err);
+            }
+            done();
+          });
+      });
+    });
+  });
+  describe("#GET /api/getAllGroupMembersByID/:id", function() {
+    context("when Id group return all members of group ", function() {
+      it("should return group members", function(done) {
+        this.timeout(10000);
+        request(server.app)
+          .get("/api/getAllGroupMembersByID/5d46a9c76533f31bb00a961c")
+          .expect(200)
+          .end((err, res) => {
+            if (err) {
+              done(err);
+            }
+            done();
+          });
+      });
+    });
+  });
+  // describe("#GET /api/getAllUsers/", function() {
+  //   context("when all users returned ", function() {
+  //     it("should return all users", function(done) {
+  //       this.timeout(10000);
+  //       request(server.app)
+  //         .get("/api/getAllUsers/")
+  //         .expect(200)
+  //         .end((err, res) => {
+  //           if (err) {
+  //             done(err);
+  //           }
+  //           done();
+  //         });
+  //     });
+  //   });
+  // });
+
+  
+
+  
+
+
 });
+
