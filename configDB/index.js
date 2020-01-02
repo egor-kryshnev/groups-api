75cf633f333ff86d0cbcfa1026a7a269b2545837
ronabet <ronabet456@gmail.com>
2020-01-02 10:05:24 +0200

    configDB comment

--- configDB/index.js
+++ configDB/index.js
@@ -2,7 +2,7 @@ var configValues = require("./config");
 
 module.exports = {
   getDbConnectionString: function() {
-    mongodb://127.0.0.1:27017/test'
+    // mongodb://127.0.0.1:27017/test'
     return "mongodb+srv://ronabet:asdgk456!@groups-erien.gcp.mongodb.net/test4?retryWrites=true&w=majority";
   }
 };
